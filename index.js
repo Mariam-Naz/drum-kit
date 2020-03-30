7ee1d8687afeaead853182ff959d895696c892bb
unknown <mariamnaz212@gmail.com>
2020-03-30 20:53:56 +0500

    add key mode also

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 for(var i=0;i<7;i++){
-document.querySelectorAll(".drum")[i].addEventListener('click',function handleClick(){
+document.querySelectorAll(".drum")[i].addEventListener('click',function(){
+
   switch(this.innerHTML){
      case 'w':
-        var audio1=new Audio('/sounds/tom-1.mp3');
+        var audio1=new Audio('./sounds/tom-1.mp3');
         audio1.play();
         break;
       case 'a':
-         var audio2=new Audio('/sounds/tom-2.mp3');
+         var audio2=new Audio('./sounds/tom-2.mp3');
          audio2.play();
          break;
       case 's':
-         var audio3=new Audio('/sounds/tom-3.mp3');
+         var audio3=new Audio('./sounds/tom-3.mp3');
          audio3.play();
          break;
       case 'd':
-         var audio4=new Audio('/sounds/tom-4.mp3');
+         var audio4=new Audio('./sounds/tom-4.mp3');
          audio4.play(); 
          break;
       case 'j':
-         var audio5=new Audio('/sounds/snare.mp3');
+         var audio5=new Audio('./sounds/snare.mp3');
          audio5.play();
          break;
       case 'k':
-         var audio6=new Audio('/sounds/crash.mp3');
+         var audio6=new Audio('./sounds/crash.mp3');
          audio6.play();
          break;
       case 'l':
-         var audio7=new Audio('/sounds/kick-bass.mp3');
+         var audio7=new Audio('./sounds/kick-bass.mp3');
          audio7.play();
          break;
       default:
-         console,log(this.innerHtml);
+         console.log(this.innerHtml);
 
 
       
@@ -39,5 +40,38 @@ document.querySelectorAll(".drum")[i].addEventListener('click',function handleCl
 }
 
 
-// var audio=new Audio('/sounds/crash.mp3');
-// audio.play();
+document.addEventListener("keydown",function(event){
+  switch(event.key){
+   case 'w':
+      var audio1=new Audio('./sounds/tom-1.mp3');
+      audio1.play();
+      break;
+    case 'a':
+       var audio2=new Audio('./sounds/tom-2.mp3');
+       audio2.play();
+       break;
+    case 's':
+       var audio3=new Audio('./sounds/tom-3.mp3');
+       audio3.play();
+       break;
+    case 'd':
+       var audio4=new Audio('./sounds/tom-4.mp3');
+       audio4.play(); 
+       break;
+    case 'j':
+       var audio5=new Audio('./sounds/snare.mp3');
+       audio5.play();
+       break;
+    case 'k':
+       var audio6=new Audio('./sounds/crash.mp3');
+       audio6.play();
+       break;
+    case 'l':
+       var audio7=new Audio('./sounds/kick-bass.mp3');
+       audio7.play();
+       break;
+    default:
+       console.log(event.key);
+
+  }
+});
